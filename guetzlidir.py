287b1f6b29f198e0a8baac3df621b42e263f143e
Evgeny Varnavskiy <varnavruz@gmail.com>
2020-11-30 16:12:41 -0500

    Fix directory walker

--- guetzlidir.py
+++ guetzlidir.py
@@ -5,12 +5,34 @@ import pathlib
 import shutil
 import sys
 import time
+from typing import Iterable
 
 import pyguetzli
 import tinify
 from PIL import Image
 
-__version__ = '0.4.1'
+SUPPORTED_FORMATS = ['jpg', 'jpeg', 'png']
+__version__ = '0.5.0'
+
+
+# Ported from: https://github.com/victordomingos/optimize-images
+def search_images(dirpath: str, recursive: bool) -> Iterable[str]:
+    if recursive:
+        for root, dirs, files in os.walk(dirpath):
+            for f in files:
+                if not os.path.isfile(os.path.join(root, f)):
+                    continue
+                extension = os.path.splitext(f)[1][1:]
+                if extension.lower() in SUPPORTED_FORMATS:
+                    yield os.path.join(root, f)
+    else:
+        with os.scandir(dirpath) as directory:
+            for f in directory:
+                if not os.path.isfile(os.path.normpath(f)):
+                    continue
+                extension = os.path.splitext(f)[1][1:]
+                if extension.lower() in SUPPORTED_FORMATS:
+                    yield os.path.normpath(f)
 
 
 def guetzlize_file(inpath, outpath, quality, minsize):
@@ -102,25 +124,26 @@ def main():
     totalsaved = 0
     totalsavedkb = 0
     c = 0
-    supported = ('.jpg', '.jpeg', '.png')
     parser = argparse.ArgumentParser(description='This tool will recursively optimize jpeg files')
     parser.add_argument('srcpath', metavar='srcpath', type=str, help='Source directory')
     parser.add_argument('dstpath', metavar='dstpath', type=str, help='Destination directory')
     parser.add_argument('-m', '--minsize', help='minimum file size in kilobytes', type=int, default=100)
     parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-q', '--quality', metavar='dstpath', type=int, default=90, help='JPEG quality, default 90')
+    group.add_argument('-q', '--quality', metavar='quality', type=int, default=90, help='JPEG quality, default 90')
     group.add_argument('-t', '--tinypng', help='TinyPNG API key', type=str)
+    parser.add_argument('-nr', '--no-recursion', action='store_true',
+                        help="Don't recurse through subdirectories.")
     args = parser.parse_args()
     # print("The arguments are: ", str(sys.argv))
     print('guetzlidir', __version__)
     if os.name == 'nt':
         # We strip nasty mess if trailing slash and quotes used
-        srcpath = pathlib.PureWindowsPath(args.srcpath.rstrip("\""))
-        dstpath = pathlib.PureWindowsPath(args.dstpath.rstrip("\""))
+        srcpath = os.path.abspath(pathlib.PureWindowsPath(args.srcpath.rstrip("\"")))
+        dstpath = os.path.abspath(pathlib.PureWindowsPath(args.dstpath.rstrip("\"")))
     else:
-        srcpath = pathlib.PurePosixPath(args.srcpath)
-        dstpath = pathlib.PurePosixPath(args.dstpath)
+        srcpath = os.path.abspath(pathlib.PurePosixPath(args.srcpath))
+        dstpath = os.path.abspath(pathlib.PurePosixPath(args.dstpath))
     if srcpath == dstpath:
         print('Input directory is same as output directory')
         exit(1)
@@ -129,29 +152,32 @@ def main():
         print('No such directory or not readable')
         sys.exit(1)
     print('Saving to', dstpath)
+    if args.no_recursion:
+        print('Processing non-recursively starting from', srcpath)
+        recursive = False
+    else:
+        print('Processing recursively starting from', srcpath)
+        recursive = True
     if not os.access(dstpath, os.W_OK):
         print('No such directory or not writable')
         sys.exit(1)
-    for subdir, dirs, files in os.walk(srcpath):
-        for filename in files:
-            filepath = subdir + os.sep + filename.lower()
-            if filepath.endswith(supported):
-                outdir = subdir.replace(str(srcpath), str(dstpath))
-                if not os.path.exists(outdir):
-                    os.mkdir(outdir)
-                outpath = outdir + os.sep + filename.lower()
-                if not args.tinypng:
-                    result = guetzlize_file(filepath, outpath, args.quality, args.minsize)
-                    if result > 0:
-                        c += 1
-                        totalsaved = totalsaved + result
-                elif args.tinypng:
-                    tinify.key = args.online
-                    result = tinypngize_file(filepath, outpath, args.minsize)
-                    if result > 0:
-                        c += 1
-                        totalsaved = totalsaved + result
-                totalsavedkb = totalsaved / 1024
+    outdir = os.path.abspath(dstpath)
+    for filepath in search_images(str(srcpath), recursive=recursive):
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        outpath = outdir + os.sep + os.path.basename(filepath).lower()
+        if not args.tinypng:
+            result = guetzlize_file(filepath, outpath, args.quality, args.minsize)
+            if result > 0:
+                c += 1
+                totalsaved = totalsaved + result
+        elif args.tinypng:
+            tinify.key = args.online
+            result = tinypngize_file(filepath, outpath, args.minsize)
+            if result > 0:
+                c += 1
+                totalsaved = totalsaved + result
+        totalsavedkb = totalsaved / 1024
 
     if totalsavedkb > 0:
         print(c, 'files processed', 'in', round((time.time() - start_time), 2), 'saving', round(totalsavedkb, 2), 'KB')
